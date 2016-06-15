43b944fbea28a57cb472f3b8972b2cc8df5b2678
戴超 <965524749@qq.com>
2016-06-15 17:15:45 +0800

    extension: UIImage

--- CYSwiftFramework/Utils/Extension/UIImage+CY.swift
+++ CYSwiftFramework/Utils/Extension/UIImage+CY.swift
@@ -37,7 +37,52 @@ extension UIImage {
         }
         
         return UIImage()
+    }
+    
+    var width: CGFloat {
+        return self.size.width
+    }
+    
+    var height: CGFloat {
+        return self.size.height
+    }
+    
+    /**
+     *  压缩图片,并返回压缩后的图片(按比例压缩)
+     */
+    func imageCompressWithTargetWidth(targetWidth: CGFloat) -> UIImage {
+        let targetHeight = (targetWidth / self.width) * self.height
         
+        UIGraphicsBeginImageContext(CGSizeMake(targetWidth, targetHeight))
+        
+        self.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    /**
+     *  返回一张模糊的图片 (使用CPU处理)
+     *  value: 模糊程度
+     *  return: 返回处理后的图片
+     */
+    func blurImage(value: NSNumber) -> UIImage {
+        let context = CIContext(options: [kCIContextUseSoftwareRenderer: true])
+        let ciImage = CoreImage.CIImage(image: self)
+        let blurFilter = CIFilter(name: "CIGaussianBlur")
+        blurFilter?.setValue(ciImage, forKey: kCIInputImageKey)
+        blurFilter?.setValue(value, forKey: "inputRadius")
+        
+        if blurFilter?.outputImage == nil || ciImage == nil {
+            return UIImage()
+        }
+        
+        let imageRef = context.createCGImage((blurFilter?.outputImage)!, fromRect: (ciImage?.extent)!)
+        let newImage = UIImage(CGImage: imageRef)
+        
+        return newImage
     }
     
 }
